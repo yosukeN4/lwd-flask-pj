863709a6c17045c3f4bc624ac8907adce7984369
yosukeN <yousken72@gmail.com>
2025-10-07 20:23:00 +0900

    add resurces and function

--- app.py
+++ app.py
@@ -5,15 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, WorlDADA!'
 
-@app.route('/test')
+@app.route('/movies', methods=["GET"])
 def hello():
-    return 'This is test page!!'
+    return 'This is test page!! Can you see this?'
+
+@app.route('/movies/exists', methods=["GET"])
+def movie_exists():
+    return 'This movie exists!! Can you see this?'
 
 @app.route("/index")
 def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)
